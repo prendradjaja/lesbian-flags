8ec7af17c6cf1aa93d38885be22c0ea6af71d924
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-11-23 20:09:38 -0600

    Use strict & move words to separate file

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const $ = s => document.querySelector(s);
 
 const colors = [
@@ -8,92 +10,6 @@ const colors = [
   '#d162a4',
   '#b55690',
   '#a30262',
-];
-
-const words = [
-  // From original post + plurals
-  'alien',
-  'aliens',
-  'baseball',
-  'baseballs',
-  'bean',
-  'beans',
-  'bees',
-  'lesbian',
-  'lesbians',
-  'lilnas',
-  'nananana',
-  'snail',
-  'lesbians',
-
-  // Pride
-  'enbies',
-  'bi',
-
-  // Food
-  'ananas',
-  'anise',
-  'basil',
-  'blini',
-  'naan',
-
-  // Animals
-  'baleen',
-  'banana',
-  'bananas',
-  'eel',
-  'eels',
-  'seal',
-  'seals',
-
-  // Math
-  'abelian',
-  'sine',
-
-  // Music
-  'abba',
-  'bassline',
-  'basslines',
-
-  // Other
-  'aisle',
-  'aisles',
-  'alias',
-  'aliases',
-  'alibi',
-  'alibis',
-  'ansible',
-  'bale',
-  'bales',
-  'balsa',
-  'bell',
-  'belle',
-  'belles',
-  'bells',
-  'biennial',
-  'ease',
-  'easel',
-  'easels',
-  'eases',
-  'enable',
-  'enables',
-  'lens',
-  'lenses',
-  'libel',
-  'linen',
-  'linens',
-  'nail',
-  'nails',
-  'nasal',
-  'nine',
-  'nines',
-  'sane',
-  'sea',
-  'seas',
-  'sensei',
-  'senseis',
-  'sensible',
-  'sienna',
 ];
 
 const inputEl = $('input');
